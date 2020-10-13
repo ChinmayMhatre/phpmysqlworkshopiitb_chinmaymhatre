b11587e20bdf07a30f3126c03e81838ccec3a7b4
Chinmay <chinmaymhatre12@gmail.com>
2020-10-13 21:04:23 +0530

    changes

--- day6,7/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('connect.php');
-$student_id = $_GET['_id'];
-$query = "DELETE FROM students WHERE id=$student_id"; 
-$result = mysqli_query($conn,$query) or die ();
-header("Location: adminhome.php"); 
-?>
